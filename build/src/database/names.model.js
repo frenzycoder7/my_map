2a21b61f01277b313d574fcfd407361b23350268
Ubuntu <ubuntu@ip-172-26-14-99.ap-south-1.compute.internal>
2023-03-26 14:39:48 +0530

    removed .env and build

--- build/src/database/names.model.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LocationModelName = exports.UserModelName = void 0;
-exports.UserModelName = 'User';
-exports.LocationModelName = 'Location';
-//# sourceMappingURL=names.model.js.map
--- build/src/types/user_types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DELETED = exports.INACTIVE = exports.ACTIVE = exports.PENDING = void 0;
-exports.PENDING = 'PENDING';
-exports.ACTIVE = 'ACTIVE';
-exports.INACTIVE = 'INACTIVE';
-exports.DELETED = 'DELETED';
-//# sourceMappingURL=user_types.js.map
